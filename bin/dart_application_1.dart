b10eca2e76bcac42e5307127a88c0819a1388d69
mykd-and <alwaysga86@gmail.com>
2023-05-11 14:59:11 +0900

    dart study

---
+++ bin/dart_application_1.dart
@@ -0,0 +1,18 @@
+void main(){
+  int age = 38;
+  double height = 173.1;
+  age += 1;
+  print(age);
+
+}
+void basic1() {
+  String name="kd";
+  String nick="mykd";
+  print('name : $name / nick : $nick');
+  
+}
+
+void basic(){
+  var name = "Kim";
+  var age = 38;
+}
